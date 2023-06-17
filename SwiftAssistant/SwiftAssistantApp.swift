067c92220d7ba3b79ff444ff044c9742167a3556
Alphonso Sensley II <alphonso.sensleyjr@gmail.com>
2023-06-17 13:00:27 -0700

    Initial Commit

---
+++ SwiftAssistant/SwiftAssistantApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftAssistantApp.swift
+//  SwiftAssistant
+//
+//  Created by Alphonso Sensley II on 6/17/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftAssistantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
