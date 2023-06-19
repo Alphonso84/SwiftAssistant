d7776f15380fc2a27179230ade0bf3094b923786
Alphonso Sensley II <alphonso.sensleyjr@gmail.com>
2023-06-18 20:05:06 -0700

    update sidebar

---
+++ SwiftAssistant/Views/Components/SideBarSeparator.swift
@@ -0,0 +1,18 @@
+//
+//  SideBarSeparator.swift
+//  SwiftAssistant
+//
+//  Created by Alphonso Sensley II on 6/18/23.
+//
+
+import SwiftUI
+
+struct SideBarSeparator: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SideBarSeparator()
+}
